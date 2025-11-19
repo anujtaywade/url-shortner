1988a0b9383c8fc95833a000803da7fb3f2a74fd
anuj taywade <anujtaywade456@gmail.com>
2025-11-19 21:56:53 +0530

    shwoimg error while testing

--- app/api/shorten/route.tsx
+++ app/api/shorten/route.tsx
@@ -1,12 +1,51 @@
 
+import { nanoid } from "nanoid";
+import next from "next";
 import { NextResponse } from "next/server";
-import {PrismaClient} from '@prisma/client';
+import {prisma} from '@/lib/db'
+
 
 export async function POST(req : Request){
     try {
-        const prisma =new PrismaClient()
-        const url = req.body
+       
+        const {url } = await req.json() 
+        
+        if(!url || (!url.startsWith('http://') && !url.startsWith('https://')))
+            { return NextResponse.json({
+        message : "url does not start with http or https"},{status : 400})}
+        
+
+        try {
+            new URL(url);
+        } catch (error) {
+            return NextResponse.json({
+                message : "Enter valid Url "
+            },{status : 400})
+            
+        }
+
+        const shortUrl = nanoid(7)
+
+        const newLink = await prisma.project.create({
+            data : {
+                originalUrl : url ,
+                shortUrl : shortUrl,
+            
+            }
+        })
+
+        return NextResponse.json({
+            message : "url shortned successful",
+            data : newLink,
+        },{status : 201})
+        
+
+
     } catch (error) {
+        console.log("post error",error)
+        return NextResponse.json({
+            message : "server error",
+        },{status : 500}) 
         
     }
 }
